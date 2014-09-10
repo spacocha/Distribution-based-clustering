3fc40b53d6e5c0eba9d764ffbaf631fb8d118d6f
Sarah Preheim <spacocha@mit.edu>
2014-09-09 22:35:10 -0400

    Divert infor to log file, require OTU list output name as input and print list to it

--- distribution_clustering_v2.0.py
+++ distribution_clustering_v2.0.py
@@ -41,7 +41,6 @@ def hamdist(str1, str2):
 def findlowest(str1, str2):
       (aligndist)=hamdist(str1,str2)
       if dounaligned:
-         print "Finding lowest between aligned and unaligned"
          ustr1=str1.replace('-','')
          ustr2=str2.replace('-','')
          (ualigndist)=hamdist(ustr1, ustr2)
@@ -56,8 +55,9 @@ def runchisq(i1, i2, OTUarray, reps):
    running chi2_contingency, getting the residuals
    and testing if they are too small and need to be simulated"""
    #limit to only the two that are tested
-   print "Starting runchisq with these idexes"
-   print i1, i2
+   log.write("Starting runchisq with these idexes\n")
+   pstring="%d %d\n" % (i1, i2)
+   log.write(pstring)
    preparearray=OTUarray[ [i1,i2] ]
    #grab only the columns that have values in either
    testarray=preparearray[:, np.where((preparearray != 0).sum(axis=0) != 0)]
@@ -67,7 +67,10 @@ def runchisq(i1, i2, OTUarray, reps):
          for k in j:
             L.append(k)
    #THIS WORKS WITH L!!!!
-   print L
+   pstring=str(L)
+   log.write(pstring)
+   log.write("\n")
+
    if len(L):
       if len(L) == 2:
          return(1)
@@ -85,13 +88,13 @@ def runchisq(i1, i2, OTUarray, reps):
             percent=newg5/newall
          if percent < 0.80:
             #check to see if the parent OTU i1 has a stored similated value
-            print "percent too low, simulate"
+            log.write("percent too low, simulate\n")
             res= chisqtest(m,simulate=True, B=reps)
-            print "Done chisq.test with simulate"
+            log.write("Done chisq.test with simulate\n")
             gc.collect()
             return(res[2][0])
          else:
-            print "Chisq test ok; return nonsim chisq"
+            log.write("Chisq test ok; return nonsim chisq\n")
             gc.collect()
             return(resnosim[2][0])
       
@@ -115,13 +118,14 @@ def assignOTU(distancecriteria, abundancecriteria, pvaluecutoff, existingOTUalig
    #the x=str(alignment[0].seq) and the index of the OTU to search for
 
    #search the existing OTUs for the 10 closests
-   print "This is the current index"
-   print i
+   pstring="This is the current index: %d\n" % (i) 
+   log.write(pstring)
    L=[]
    for y in existingOTUalignment:
       d=findlowest(x, str(y.seq))
       #append this to the current list of close sequences
-      L.append((d, y.name))
+      if d < distancecriteria:
+         L.append((d, y.name))
       
    #sort the list by the first value of the tuple
    sortedL=sorted(sorted(L, key=lambda tup: tup[1]))
@@ -138,22 +142,22 @@ def assignOTU(distancecriteria, abundancecriteria, pvaluecutoff, existingOTUalig
       actualjindex=jindex[0][0]
       #does it fit the distance criteria
       if sortedL[j][0] < distancecriteria:
-         print "Passed distance criteria"
+         log.write("Passed distance criteria\n")
          #ok to continue
          #does it fit the distance criteria
          if alldata[actualjindex][-1] >= alldata[i][-1]*abundancecriteria:
-            print "Passed abundance criteria"
+            log.write("Passed abundance criteria\n")
             #it satisfies the abundance criteria
             #get the pvalue
             pval=runchisq(i, actualjindex, OTUtable1, 10000)
             if pval < pvaluecutoff:
-               print "Did not pass pvaluecutoff; look for another"
+               log.write("Did not pass pvaluecutoff; look for another\n")
                #if it's outside of the cutoff, its significant
                #assign to another 
                #print to log if verbose
                continue
             else:
-               print "Passed pvalue: merge"
+               log.write("Passed pvalue: merge\n")
                #this is the OTU to merge with
                #print to the log if verbose
                tup=(actualjindex, pval, sortedL[j][0])
@@ -161,22 +165,22 @@ def assignOTU(distancecriteria, abundancecriteria, pvaluecutoff, existingOTUalig
          else:
             #get the next closest one and print to the log if verbose
             #print to log if verbose
-            print "Did not pass abundance criteria"
+            log.write("Did not pass abundance criteria\n")
             continue
       else:
-         print "Did not pass distance criteria"
+         log.write("Did not pass distance criteria\n")
          tup=('NA', 'NA', 'NA')
          return('not merged', tup)
    else:
       #this will be something to do if you don't find an OTU to merge into
       #not sure if this is necessary
-      print "Exit without break"
+      log.write("Exit without break\n")
       tup=('NA', 'NA', 'NA')
       return('not merged', tup)
 
 
 def workthroughtable (distancecriteria, abundancecriteria, pvaluecutoff, OTUtable1, OTUtable2, alignment, onlyOTUs):
-   print "Start workingthroughtable"
+   log.write("Start workingthroughtable\n")
    new_col = OTUtable1.sum(1)[...,None]
    all_data = np.append(OTUtable1, new_col, 1)
 
@@ -185,61 +189,81 @@ def workthroughtable (distancecriteria, abundancecriteria, pvaluecutoff, OTUtabl
 
    #now I can work through this from the reverse
    for nindex in reversed(rsortindex):
-      print "Starting workthough"
+      log.write("Starting workthough\n")
       #work though the index values from most to least abundant
       #nindex is the index of the next most abundant
       if 'existingOTUalignment' in locals():
-         print "OTUs exist, assignOTUs"
+         log.write("OTUs exist, assignOTUs\n")
          #OTUs exist, see if it will fit into the existingOTU set
          #this tests both the genetic and ecological similarity
       
          res=assignOTU(distancecriteria, abundancecriteria, pvaluecutoff, existingOTUalignment, OTUtable1, onlyOTUs, all_data, str(alignment[nindex].seq), nindex)
-         print "Finished assignOTU"
+         log.write("Finished assignOTU\n")
          #Work on this, I'm not sure how to do this exactly
          if res[0] == 'merged':
-            print "The result is merged"
+            log.write("The result is merged\n")
             #print out the the log that it's part of the mergeOTU
-            print "Changefrom,%s,%s,Changeto,p.value,%f,Dist,%f,Done\n" % (onlyOTUs[nindex], onlyOTUs[res[1][0]], res[1][1], res[1][2])
+            pstring="Changefrom,%s,%s,Changeto,p.value,%f,Dist,%f,Done\n" % (onlyOTUs[nindex], onlyOTUs[res[1][0]], res[1][1], res[1][2])
+            log.write(pstring)
+            listdict[onlyOTUs[res[1][0]]].append(onlyOTUs[nindex])
          else:
             #nothing came back, so create it as a new OTU
-            print "the result is nothing"
-            print "%s is a parent,Done\n" % (onlyOTUs[nindex])
+            log.write("the result is nothing\n")
+            pstring="%s is a parent,Done\n" % (onlyOTUs[nindex])
+            log.write(pstring)
             existingOTUalignment.append(alignment[nindex])
+            listdict[onlyOTUs[nindex]] = [onlyOTUs[nindex]]
       else:
          #theres nothing to merge with, it's a parent (it's the first one)
-         print "No OTUs exist, begin"
-         print "%s is a parent,Done\n" % (onlyOTUs[nindex])
+         log.write("No OTUs exist, begin\n")
+         string="%s is a parent,Done\n" % (onlyOTUs[nindex])
+         log.write(string)
          #create the existingOTUalignment to search through
          existingOTUalignment=alignment[nindex:nindex+1]
+         listdict[onlyOTUs[nindex]] = [onlyOTUs[nindex]]
 
 if __name__ == '__main__':
    parser = argparse.ArgumentParser(description='Create OTUs using ecological and genetic information (DBC version 2.0)')
    parser.add_argument('OTUtablefile', help='OTU table input')
    parser.add_argument('alignmentfile', help='alignment file input')
-   parser.add_argument('-o', '--output', default=sys.stdout, type=argparse.FileType('w'), help='output file (default stdout)')
+   parser.add_argument('outlistfile', help='list output file name')
+   parser.add_argument('-o', '--output', default=sys.stdout, type=str, help='output file (default stdout)')
    parser.add_argument('-d', '--dist_cutoff', type=float, default=0.1, help='maximum genetic variation allowed to be within the same population (i.e. OTU)')
    parser.add_argument('-k', '--k_fold', default=0, type=float, help='abundance criteria: existing OTU rep must have at least k-fold increase over the candidate sequence to be joined (use 10 for seq error only)')
    parser.add_argument('-p', '--pvalue', type=float, default=0.0005, help='pvalue cut-off: this could vary depending on the total number of libraries')
    parser.add_argument('-u', '--unaligned', type=str, default=False, help='use the unaligned sequence to correct alignment issues')
    args = parser.parse_args()
-   print(datetime.now())
-   dounaligned=args.unaligned
+   log =open(args.output, 'w')
+   timestamp=str(datetime.now())
+   string="%s\n" % (timestamp)
+   log.write(string)
+   log.write("This is a test\n")
    global dounaligned
+   dounaligned=args.unaligned
    if 'dounaligned' in globals():
-      print "yes in global alignment"
+      log.write("yes in global alignment\n")
    else:
-      print "No unaligned in globals"
+      log.write("No unaligned in globals\n")
 
    if 'dounaligned' in locals():
-      print "Yes in locals"
+      log.write("Yes in locals\n")
    else:
-      print "Not in locals either"
+      log.write("Not in locals either\n")
 
    table = np.genfromtxt(args.OTUtablefile, comments="#")
    OTUtable1=table[1:,1:]
    OTUtable2 = np.genfromtxt(args.OTUtablefile, comments="#", names=True, dtype=None)
    alignment = AlignIO.read(args.alignmentfile, "fasta")
    onlyOTUs=OTUtable2['OTU']
+   listdict=dict()
    workthroughtable (args.dist_cutoff, args.k_fold, args.pvalue, OTUtable1, OTUtable2, alignment, onlyOTUs)
-   print "Finished distribution-based clustering"
-   print(datetime.now())
+   log.write("Finished distribution-based clustering\n")
+   outlist =open(args.outlistfile, 'w')
+   for x in listdict:
+      outlist.write("\t".join(listdict[x]))
+      outlist.write("\n")
+
+   timestamp=str(datetime.now())
+   string="%s\n" % (timestamp)
+   log.write(string)   
+
